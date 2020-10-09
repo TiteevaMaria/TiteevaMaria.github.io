f35531eeeb9c6c2bf14faee86167556d95d1872b
TiteevaMaria <46442086+TiteevaMaria@users.noreply.github.com>
2020-10-09 20:58:46 +0300

    Add files via upload

---
+++ back-grounds/waves/custom-back-ground.js
@@ -0,0 +1,15 @@
+VANTA.WAVES({
+  el: "#custom-back-ground",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x3769,
+  shininess: 150.00,
+  waveHeight: 40.00,
+  waveSpeed: 0.50,
+  zoom: 0.8
+})
